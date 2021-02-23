9ec027afa40fe7419f0ffd2cb89419d9d5611ff9
Trung Doan <duytrung177@gmail.com>
2021-02-23 15:26:28 +0700

    up project

---
+++ src/main/java/com/levunguyen/propertysource/PropertySourceApplication.java
@@ -0,0 +1,13 @@
+package com.levunguyen.propertysource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertySourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertySourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/levunguyen/propertysource/PropertySourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levunguyen.propertysource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertySourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
